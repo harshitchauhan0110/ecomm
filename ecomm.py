9365c3c175802ed8753700f8ca4bd004039780e6
harshitchauhan0110 <harshitscv101@gmail.com>
2025-09-03 21:51:55 +0530

    Initial commit

---
+++ ecomm.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+def main():
+    st.title("This is my streamlit app for ecommerce that i have")
+    st.sidebar.title("Upload your File")
+
+    uploaded_file = st.sidebar.file_uploader("Upload your File", type=["csv","xlsx"])
+    try :
+        if uploaded_file  is not None:
+            try:
+                if uploaded_file.name.endswith(".csv"):
+                    data = pd.read_csv(uploaded_file)
+                else :
+                    data = pd.read_excel(uploaded_file)
+
+                st.sidebar.success("File Uploaded Successfully")
+
+                st.subheader("data overview")
+                st.dataframe(data.head())
+
+                st.subheader("Basic information of the data")
+                st.write("shape of the data", data.shape)
+                st.write("columns in my", data.columns)
+                st.write("missing valu", data.isnull().sum())
+                
+                st.subheader("I will show you stats of the data")
+                st.write(data.describe())
+
+            except Exception as e:
+                print("it will handle if thing gets wrong",e)
+        else:
+            pass
+    except Exception as e :
+        print("error is {e}")
+
+
+if __name__ == "__main__":
+    main()
